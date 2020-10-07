95800058acbc80e6c84b8158142b35293e76ef19
Antony Rafael <69513020+antony-raf@users.noreply.github.com>
2020-10-07 11:18:30 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Antony Rafael!"
+    return "Meu primeiro Pipeline!"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
